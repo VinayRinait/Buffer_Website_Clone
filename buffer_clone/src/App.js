5568fe7fd67528f807ecb26fddaf1669cafed97f
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2022-12-14 22:11:45 +0530

    vivek

--- buffer_clone/src/App.js
+++ buffer_clone/src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Admin from "./components/Admin";
-import Analytic from "./pages/Analytic";
-import { HomePage } from "./pages/HomePage";
 
+import "./App.css";
 function App() {
   return (
     <>
       {/* <Admin /> */}
       {/* <Analytic /> */}
-      <HomePage/>
+      {/* <HomePage/> */}
     </>
   );
 }
