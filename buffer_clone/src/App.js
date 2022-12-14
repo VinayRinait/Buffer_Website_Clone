331ce0070e8854bc4c5b0f86e0fe7f597720425e
hemuu12 <harisibgh.bisht2001@gmail.com>
2022-12-14 22:15:25 +0530

    fw19_1112

--- buffer_clone/src/App.js
+++ buffer_clone/src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Admin from "./components/Admin";
-import Analytic from "./pages/Analytic";
+
 
 function App() {
   return (
     <>
       {/* <Admin /> */}
-      <Analytic />
+      {/* <Analytic /> */}
+      {/* <Publishing/> */}
     </>
   );
 }
