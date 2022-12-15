40c70332d431ff468156977595d6d8d06b47bcde
Vivek Maurya <vivekmauryarkt1998@gmail.com>
2022-12-15 20:04:46 +0530

    Vivek day2

--- buffer_clone/src/App.js
+++ buffer_clone/src/App.js
@@ -1,11 +1,12 @@
 
 import "./App.css";
+import { HomePage } from "./pages/HomePage";
 function App() {
   return (
     <>
       {/* <Admin /> */}
       {/* <Analytic /> */}
-      {/* <HomePage/> */}
+      <HomePage/>
     </>
   );
 }
