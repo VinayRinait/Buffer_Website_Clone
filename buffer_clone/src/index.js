6066cb7969a9899f9afa1922638bd1ea997c59eb
Priyanshu7250 <kpriyanshu06@gmail.com>
2022-12-15 11:02:58 +0530

    priyanshu

--- buffer_clone/src/index.js
+++ buffer_clone/src/index.js
@@ -1,16 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import {BrowserRouter} from "react-router-dom"
+import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <StyledEngineProvider injectFirst>
+  <CssVarsProvider>
+  <BrowserRouter>
+  
   <ChakraProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+ 
+ <React.StrictMode>
+    <App />
+  </React.StrictMode>
+ 
   </ChakraProvider>
+  
+  </BrowserRouter>
+  </CssVarsProvider>
+  </StyledEngineProvider>
+
+
+
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
