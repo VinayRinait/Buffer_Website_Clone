1b19074e73d5775624c1b8b3eb8a0fc8acbf38cf
Priyanshu7250 <kpriyanshu06@gmail.com>
2022-12-17 21:17:57 +0530

    added

--- buffer_clone/src/App.js
+++ buffer_clone/src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import Instagram from "./pages/Instagram";
-import Userprofile from "./pages/Userprofile";
-import { Navbar } from "./components/Navbar";
 
-// import "./App.css";
+
 
 import "./App.css";
-import { HomePage } from "./pages/HomePage";
+
 function App() {
   return (
     <div>
-      <Navbar />
-      {/* <Instagram /> */}
-      <Userprofile />
       
     </div>
   );
